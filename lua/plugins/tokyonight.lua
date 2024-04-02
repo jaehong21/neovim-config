82732787439d5f464476d41828a943ea988a0546
jaehong21 <dev@jaehong21.com>
2024-04-03 00:56:18 +0900

    style: transparent tokyonight

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -13,9 +13,17 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+           sidebars = "transparent",
+           floats = "transparent",
+        }
+      })
+
       vim.cmd("colorscheme tokyonight")
     end,
     opts = {},
-  }
+  },
 }
 
