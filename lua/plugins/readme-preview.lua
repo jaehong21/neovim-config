bdb6e85813821026f7c2df29e6e4b5bc33cfcc3f
jaehong21 <dev@jaehong21.com>
2024-08-06 00:35:39 +0900

    feat: add readme-preview

--- lua/plugins/readme-preview.lua
+++ lua/plugins/readme-preview.lua
@@ -1,8 +1,17 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		main = "render-markdown",
+		opts = {},
+		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	},
 }
