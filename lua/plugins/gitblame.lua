6d324bb6fca653e0e873fc3866efc513198609eb
jaehong21 <dev@jaehong21.com>
2024-09-02 20:21:23 +0900

    feat: add vim-fugitive

--- lua/plugins/gitblame.lua
+++ lua/plugins/gitblame.lua
@@ -1,25 +1,30 @@
 return {
-	"f-person/git-blame.nvim",
-	config = function()
-		local gitblame = require("gitblame")
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"f-person/git-blame.nvim",
+		config = function()
+			local gitblame = require("gitblame")
 
-		gitblame.setup({
-			enabled = true,
-			message_template = "<author> • <summary> • <date> • <sha>",
-			message_when_not_committed = "Not Committed Yet",
-			ignored_filetypes = {},
-			delay = 100, -- delay in ms
-			schedule_event = "CursorMoved", -- options: CursorMoved, CursorHold
-			clear_event = "CursorMovedI", -- options: CursorMovedI, CursorHoldI
-			use_blame_commit_file_urls = true,
-		})
+			gitblame.setup({
+				enabled = true,
+				message_template = "<author> • <summary> • <date> • <sha>",
+				message_when_not_committed = "Not Committed Yet",
+				ignored_filetypes = {},
+				delay = 100, -- delay in ms
+				schedule_event = "CursorMoved", -- options: CursorMoved, CursorHold
+				clear_event = "CursorMovedI", -- options: CursorMovedI, CursorHoldI
+				use_blame_commit_file_urls = true,
+			})
 
-		-- require("lualine").setup({
-		-- 	sections = {
-		-- 		lualine_c = {
-		-- 			{ gitblame.get_current_blame_text, cond = gitblame.is_blame_text_available },
-		-- 		},
-		-- 	},
-		-- })
-	end,
+			-- require("lualine").setup({
+			-- 	sections = {
+			-- 		lualine_c = {
+			-- 			{ gitblame.get_current_blame_text, cond = gitblame.is_blame_text_available },
+			-- 		},
+			-- 	},
+			-- })
+		end,
+	},
 }
