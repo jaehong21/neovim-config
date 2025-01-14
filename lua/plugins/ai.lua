fcdbbf7f4c7291208037010e85e7b0274f4004df
jaehong21 <dev@jaehong21.com>
2025-01-14 23:56:16 +0900

    feat: add avante and blink.cmp

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,70 @@
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		opts = {
+			filetypes = { yaml = true },
+		},
+		config = true,
+	},
+
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = "*", -- '*' for latest | 'false' for release
+		build = "make",
+		-- https://github.com/yetone/avante.nvim/blob/main/lua/avante/config.lua
+		opts = {
+			---@alias avante.nvim.Provider "claude" | "openai" | "azure" | "gemini" | "vertex" | "cohere" | "copilot" | string
+			provider = "copilot",
+			openai = {
+				-- api_key_name = "OPENROUTER_API_KEY",
+				-- endpoint = "https://openrouter.ai/api/v1",
+				-- model = "anthropic/claude-3.5-sonnet", -- "deepseek/deepseek-chat" | "openai/chatgpt-4o-latest"
+				api_key_name = "OPENAI_API_KEY",
+				endpoint = "https://api.openai.com/v1", -- default
+				model = "gpt-4o", -- default
+			},
+			copilot = {
+				-- endpoint = "https://api.githubcopilot.com", -- default
+				-- model = "gpt-4o-2024-08-06" -- default
+			},
+			file_selector = {
+				provider = "fzf", -- 'native' | 'fzf' (fzf-lua) | 'telescope' | string
+				provider_opts = {},
+			},
+		},
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+
+			-- optional dependencies below
+			"ibhagwan/fzf-lua", -- for `file_selector`
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			-- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = { file_types = { "markdown", "Avante" } },
+				ft = { "markdown", "Avante" },
+			},
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = { insert_mode = true },
+						-- use_absolute_path = true, -- for Windows users
+					},
+				},
+			},
+		},
+	},
+}
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,7 +1,19 @@
 return {
 	{
+		"saghen/blink.compat",
+		version = "*",
+		lazy = true,
+		opts = {
+			sources = {
+				compat = { "avante_commands", "avante_mentions", "avante_files" },
+			},
+		},
+	},
+
+	{
 		"saghen/blink.cmp",
 		dependencies = {
+			"saghen/blink.compat",
 			-- optional: provides snippets for the snippet source
 			"rafamadriz/friendly-snippets",
 			{ "L3MON4D3/LuaSnip", version = "v2.*" },
@@ -11,20 +23,57 @@ return {
 		---@type blink.cmp.Config
 		opts = {
 			-- https://cmp.saghen.dev/configuration/keymap.html#presets
-			keymap = { preset = "super-tab" }, -- "default" | "super-tab" | "enter"
-
+			keymap = {
+				preset = "super-tab", -- "default" | "super-tab" | "enter"
+				-- cmdline = { preset = "super-tab" },
+			},
 			appearance = {
 				nerd_font_variant = "mono", -- "normal" | "mono"
-
 				use_nvim_cmp_as_default = false,
 			},
 
-			completion = { ghost_text = { enabled = true } },
-
+			completion = {
+				accept = {
+					auto_brackets = {
+						enabled = true,
+						kind_resolution = { enabled = true },
+						semantic_token_resolution = { enabled = true },
+					},
+				},
+				ghost_text = { enabled = true },
+			},
 			snippets = { preset = "luasnip" },
 			sources = {
 				-- you can extend it elsewhere in your config, without redefining it, due to `opts_extend`
-				default = { "lsp", "snippets", "buffer", "path" },
+				default = {
+					"lsp",
+					"snippets",
+					"buffer",
+					"path",
+					"avante_commands",
+					"avante_mentions",
+					"avante_files",
+				},
+				providers = {
+					avante_commands = {
+						name = "avante_commands",
+						module = "blink.compat.source",
+						score_offset = 90, -- show at a higher priority than lsp
+						opts = {},
+					},
+					avante_files = {
+						name = "avante_commands",
+						module = "blink.compat.source",
+						score_offset = 100, -- show at a higher priority than lsp
+						opts = {},
+					},
+					avante_mentions = {
+						name = "avante_mentions",
+						module = "blink.compat.source",
+						score_offset = 1000, -- show at a higher priority than lsp
+						opts = {},
+					},
+				},
 			},
 			-- https://cmp.saghen.dev/configuration/reference.html#signature
 			signature = { enabled = true },
@@ -44,72 +93,4 @@ return {
 			-- end, { expr = true })
 		end,
 	},
-
-	{
-		"github/copilot.vim",
-		lazy = false,
-		cmd = "Copilot",
-	},
-	--[[ {
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lua",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"saadparwaiz1/cmp_luasnip",
-			"L3MON4D3/LuaSnip",
-		},
-		config = function()
-			local cmp = require("cmp")
-			vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				window = {
-					-- completion = cmp.config.window.bordered(),
-					-- documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-					-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-					-- ["<C-Space>"] = cmp.mapping.complete(),
-					-- ["<C-e>"] = cmp.mapping.abort(),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "nvim_lua" },
-					{ name = "luasnip" }, -- For luasnip users.
-					-- { name = "orgmode" },
-				}, {
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			})
-
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "buffer" },
-				}),
-			})
-
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-					{ name = "cmdline" },
-				}),
-			})
-			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		end,
-	}, ]]
 }
