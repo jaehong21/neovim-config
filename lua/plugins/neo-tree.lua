0cc996cf4184574ab671305f49f5b052e3b3ffda
jaehong21 <dev@jaehong21.com>
2025-01-17 19:45:04 +0900

    refactor: merge oil config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,21 @@
 return {
-	-- {
-	-- 	"stevearc/oil.nvim",
-	-- 	opts = {},
-	-- 	-- Optional dependencies
-	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- },
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		enabled = true,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
+			require("oil").setup({
+				default_file_explorer = true,
+			})
+		end,
+	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
+		enabled = true,
 		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
@@ -14,46 +23,45 @@ return {
 			"MunifTanjim/nui.nvim",
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
-		config = function()
-			require("neo-tree").setup({
-				window = {
-					position = "left",
-					width = 35,
-				},
-				filesystem = {
-					filtered_items = {
-						visible = false,
-						always_show = {
-							".gitignore",
-							".golangci.yml",
-							".dockerignore",
-							".github",
-							".gitea",
-							".gitlab-ci.yml",
-							".circleci",
-							".pre-commit-config.yaml",
-							".goreleaser.yaml",
-							"generated",
-							".npmrc",
-							".nvmrc",
-							".yarnrc.yml",
-							"data",
-							".streamlit",
-							".pulumi",
-							".air.toml",
-							".dev.vars",
-							".env",
-							".env.local",
-							".env.development",
-							".env.production",
-						},
+		opts = {
+			window = {
+				position = "left",
+				width = 35,
+			},
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					always_show = {
+						".gitignore",
+						".golangci.yml",
+						".dockerignore",
+						".github",
+						".gitea",
+						".gitlab-ci.yml",
+						".circleci",
+						".pre-commit-config.yaml",
+						".goreleaser.yaml",
+						"generated",
+						".npmrc",
+						".nvmrc",
+						".yarnrc.yml",
+						"data",
+						".streamlit",
+						".pulumi",
+						".air.toml",
+						".dev.vars",
+						".env",
+						".env.local",
+						".env.development",
+						".env.production",
 					},
 				},
-			})
-
+			},
+		},
+		config = function(_, opts)
+			require("neo-tree").setup(opts)
 			-- Reveal current file in neotree
 			vim.keymap.set("n", "<leader>fr", ":Neotree reveal<CR>")
 		end,
-		opts = {},
 	},
 }
