8ab530d358f06c44edc19b39d2933859d387fa8e
jaehong21 <dev@jaehong21.com>
2025-01-17 18:02:40 +0900

    docs: change comment

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,10 +5,9 @@ return {
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			--   NOTE: uncomment if you want to use nvim-notify
+			-- NOTE: OPTIONAL:
+			--  `nvim-notify` is only needed, if you want to use the notification view.
+			--  If not available, we use `mini` as the fallback
 			-- "rcarriga/nvim-notify",
 		},
 		config = function()
@@ -22,25 +21,26 @@ return {
 			})
 		end,
 	},
-	-- {
-	-- 	"rcarriga/nvim-notify",
-	-- 	opts = {
-	-- 		on_open = function(win)
-	-- 			local config = vim.api.nvim_win_get_config(win)
-	-- 			config.border = "single"
-	-- 			vim.api.nvim_win_set_config(win, config)
-	-- 		end,
-	-- 	},
-	-- 	config = function(_, opts)
-	-- 		-- :h notify.setup()
-	-- 		---@diagnostic disable-next-line: undefined-field
-	-- 		require("notify").setup({
-	-- 			render = "compact",
-	-- 			stages = "fade_in_slide_out",
-	-- 			timeout = 150,
-	-- 			top_down = true,
-	-- 			on_open = opts.on_open,
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"rcarriga/nvim-notify",
+		enabled = false,
+		opts = {
+			on_open = function(win)
+				local config = vim.api.nvim_win_get_config(win)
+				config.border = "single"
+				vim.api.nvim_win_set_config(win, config)
+			end,
+		},
+		config = function(_, opts)
+			-- :h notify.setup()
+			---@diagnostic disable-next-line: undefined-field
+			require("notify").setup({
+				render = "compact",
+				stages = "fade_in_slide_out",
+				timeout = 150,
+				top_down = true,
+				on_open = opts.on_open,
+			})
+		end,
+	},
 }
