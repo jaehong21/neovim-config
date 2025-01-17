659576413274beb50f9ae363ac3a087dabc6947a
jaehong21 <dev@jaehong21.com>
2025-01-17 18:03:07 +0900

    feat: change tabline to barbar

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -1,19 +1,34 @@
 return {
-	-- {
-	-- 	"echasnovski/mini.tabline",
-	-- 	version = false,
-	-- 	config = function()
-	-- 		require("mini.tabline").setup({
-	-- 			show_icons = true,
-	-- 			set_vim_settings = true,
-	-- 			tabpage_section = "left",
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"romgrk/barbar.nvim",
+		version = "*",
+		dependencies = {
+			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+			vim.keymap.set("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", { noremap = true, silent = true })
+		end,
+		opts = { animation = false },
+	},
+	{
+		"echasnovski/mini.tabline",
+		enabled = false,
+		version = false,
+		config = function()
+			require("mini.tabline").setup({
+				show_icons = true,
+				-- set_vim_settings = true,
+				-- tabpage_section = "left",
+			})
+		end,
+	},
 	{
 		"kdheepak/tabline.nvim",
+		enabled = false,
 		dependencies = {
-			"hoob3rt/lualine.nvim",
+			"nvim-lualine/lualine.nvim",
 			"kyazdani42/nvim-web-devicons",
 		},
 		config = function()
