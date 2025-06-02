fdbe8c5be9abdfa67a92d7633fd6282f8a86a280
jaehong21 <dev@jaehong21.com>
2025-06-03 00:36:27 +0900

    feat: add which-key

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,12 +3,33 @@ return {
 	event = "VeryLazy",
 	---@type Flash.Config
 	opts = {},
-  -- stylua: ignore
-  keys = {
-    { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+	-- stylua: ignore
+	keys = {
+		-- { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+		-- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+		-- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+		-- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+		-- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    
+    -- Disable default keymaps
+		{ "s", mode = { "n", "x", "o" }, false },
+		{ "S", mode = { "n", "x", "o" }, false },
+		{ "r", mode = "o", false },
+		{ "R", mode = { "o", "x" }, false },
+		{ "<c-s>", mode = { "c" }, false },
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+
+		wk.add({
+			{
+				"<leader>s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+		})
+	end,
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -10,16 +10,36 @@ return {
 		-- vim.keymap.set({ "n", "t" }, "<leader>j", "<CMD>ToggleTerm<CR>", { noremap = true, silent = true })
 		-- vim.keymap.set({ "n", "t" }, "J", "<CMD>ToggleTerm<CR>", { noremap = true, silent = true })
 
+		local wk = require("which-key")
+
 		local Terminal = require("toggleterm.terminal").Terminal
 
 		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
-		vim.keymap.set({ "n", "t" }, "<leader>gl", function()
-			lazygit:toggle()
-		end, { noremap = true, silent = true, desc = "[G]it with [L]azygit" })
+		wk.add({
+			{
+				"<leader>gl",
+				function()
+					lazygit:toggle()
+				end,
+				desc = "[G]it with [L]azygit",
+			},
+		})
+		-- vim.keymap.set({ "n", "t" }, "<leader>gl", function()
+		-- 	lazygit:toggle()
+		-- end, { noremap = true, silent = true, desc = "[G]it with [L]azygit" })
 
 		local hibiscus = Terminal:new({ cmd = "hibiscus", hidden = true, direction = "float" })
-		vim.keymap.set({ "n", "t" }, "<leader>hb", function()
-			hibiscus:toggle()
-		end, { noremap = true, silent = true, desc = "[H]i[B]iscus" })
+		wk.add({
+			{
+				"<leader>hb",
+				function()
+					hibiscus:toggle()
+				end,
+				desc = "[H]i[B]iscus",
+			},
+		})
+		-- vim.keymap.set({ "n", "t" }, "<leader>hb", function()
+		-- 	hibiscus:toggle()
+		-- end, { noremap = true, silent = true, desc = "[H]i[B]iscus" })
 	end,
 }
