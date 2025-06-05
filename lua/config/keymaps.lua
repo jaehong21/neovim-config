1e49879dc326e2c1acf94e4162490d05f9eb3bfc
jaehong21 <dev@jaehong21.com>
2025-06-05 17:13:28 +0900

    revert: keymaps for colemak-dh layout

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,29 +1,29 @@
 local mapKey = require("utils.keymapper").mapKey
 
 -- pane navigation
--- mapKey("<C-h>", "<C-w>h") -- Left
--- mapKey("<C-j>", "<C-w>j") -- Down
--- mapKey("<C-k>", "<C-w>k") -- Up
--- mapKey("<C-l>", "<C-w>l") -- Right
+mapKey("<C-h>", "<C-w>h") -- Left
+mapKey("<C-j>", "<C-w>j") -- Down
+mapKey("<C-k>", "<C-w>k") -- Up
+mapKey("<C-l>", "<C-w>l") -- Right
 
 -- pane navigation (Colemak-DH)
-mapKey("<C-m>", "<C-w>h") -- Left
-mapKey("<C-n>", "<C-w>j") -- Down
-mapKey("<C-e>", "<C-w>k") -- Up
-mapKey("<C-i>", "<C-w>l") -- Right
+-- mapKey("<C-m>", "<C-w>h") -- Left
+-- mapKey("<C-n>", "<C-w>j") -- Down
+-- mapKey("<C-e>", "<C-w>k") -- Up
+-- mapKey("<C-i>", "<C-w>l") -- Right
 
 -- split
 mapKey("<leader>vs", "<Cmd>vsplit<CR>", "n")
 
 -- buffer
--- mapKey("<S-h>", "<Cmd>bprev<CR>", "n") -- Previous buffer
--- mapKey("<S-l>", "<Cmd>bnext<CR>", "n") -- Next buffer
+mapKey("<S-h>", "<Cmd>bprev<CR>", "n") -- Previous buffer
+mapKey("<S-l>", "<Cmd>bnext<CR>", "n") -- Next buffer
 mapKey("<leader>q", "<Cmd>bd<CR>", "n") -- Close pane
 mapKey("<leader><leader>", "<C-^>") -- Move to last buffer
 
 -- buffer navigation (Colemak-DH)
-mapKey("<S-m>", "<Cmd>bprev<CR>", "n") -- Previous buffer
-mapKey("<S-o>", "<Cmd>bnext<CR>", "n") -- Next buffer (as Shitt+i is for Insert mode, setting as Shift+o)
+-- mapKey("<S-m>", "<Cmd>bprev<CR>", "n") -- Previous buffer
+-- mapKey("<S-o>", "<Cmd>bnext<CR>", "n") -- Next buffer (as Shitt+i is for Insert mode, setting as Shift+o)
 
 -- clear search highlight
 mapKey("<leader>h", ":nohlsearch<CR>")
@@ -40,14 +40,16 @@ mapKey("_", ":vertical resize -5<CR>")
 mapKey("<leader>gh", "<Cmd>!gh repo view --web<CR>", "n")
 
 -- Colemak-DH hjkl remap
+--[[
 mapKey("n", "j", "n")
 mapKey("e", "k", "n")
--- mapKey("m", "h", "n")
--- mapKey("i", "l", "n") -- Not setting this to avoid contlict for `Insert` mode
+mapKey("m", "h", "n")
+mapKey("i", "l", "n") -- Not setting this to avoid contlict for `Insert` mode
 
 mapKey("<S-n>", "n", "n") -- Mapping Shift+n to next search result (Originally previous search result)
 
 mapKey("n", "j", "v")
 mapKey("e", "k", "v")
--- mapKey("m", "h", "v")
--- mapKey("i", "l", "v") -- Not setting this to avoid contlict for `Visual` mode
+mapKey("m", "h", "v")
+mapKey("i", "l", "v") -- Not setting this to avoid contlict for `Visual` mode
+]]
