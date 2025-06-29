f9214f012c8e2dc1db41f50a46a7dad593bdc2e4
jaehong21 <dev@jaehong21.com>
2025-06-29 18:57:49 +0900

    refactor: move vim.g.mapleader to init.lua

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,35 +1,47 @@
+-- https://lazy.folke.io/installation
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+---@diagnostic disable-next-line: undefined-field for `fs_stat`
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		"--branch=stable", -- use stable branch
+		lazyrepo,
 		lazypath,
 	})
+
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-require("config.globals")
-require("config.keymaps")
-require("config.options")
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
--- require("lazy").setup(plugins, opts)
+-- This is also a good place to setup other settings (vim.opt)
+require("config.options")
+require("config.keymaps")
+
+-- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		-- { "LazyVim/LazyVim" },
-		{ import = "plugins.lsp" },
 		{ import = "plugins" },
-	},
-	-- import/override with your plugins
-	defaults = {
-		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-		lazy = false,
-		version = false, -- always use the latest git commit
+		{ import = "lsp" },
 	},
 	checker = {
 		enabled = true, -- check for plugin updates periodically
-		notify = false, -- notify on update
-	}, -- automatically check for plugin updates
+	},
 })
