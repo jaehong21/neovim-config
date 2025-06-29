b0a7957d87d98d455e77b7fdeaf544a122c57099
jaehong21 <dev@jaehong21.com>
2025-06-29 20:10:08 +0900

    feat: add nvim-highlight-colors

---
+++ lua/plugins/highlight-colors.lua
@@ -0,0 +1,11 @@
+return {
+	"brenoprata10/nvim-highlight-colors",
+	-- https://github.com/brenoprata10/nvim-highlight-colors?tab=readme-ov-file#options
+	opts = {
+		-- https://github.com/brenoprata10/nvim-highlight-colors?tab=readme-ov-file#render-modes
+		render = "background", -- "background" | "foreground" | "virtual"
+		enable_tailwind = true,
+	},
+	-- Ensure termguicolors is enabled if not already
+	-- vim.opt.termguicolors = true
+}
