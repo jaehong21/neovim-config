be49cc435c23bf65ca1d820d9c45447200a72d10
jaehong21 <dev@jaehong21.com>
2025-07-26 23:34:09 +0900

    fix: remove oil moveAction

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -48,18 +48,6 @@ return {
 			end,
 		},
 	},
-	config = function()
-		-- https://github.com/folke/snacks.nvim/blob/main/docs/rename.md#oilnvim
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "OilActionsPost",
-			callback = function(event)
-				if event.data.actions.type == "move" then
-					---@module 'snacks'
-					Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
-				end
-			end,
-		})
-	end,
 	keys = {
 		{ "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
 	},
