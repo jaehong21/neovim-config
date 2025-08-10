dbf5ed918619364b393216b86a23c97285be40b3
jaehong21 <dev@jaehong21.com>
2025-08-10 13:32:27 +0900

    feat: support update import when rename file in oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -48,7 +48,22 @@ return {
 			end,
 		},
 	},
-	keys = {
-		{ "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
-	},
+	config = function(_, opts)
+		require("oil").setup(opts)
+		vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+
+		-- rename file with lsp
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "OilActionsPost",
+			callback = function(event)
+				if event.data.actions.type == "move" then
+					---@module 'snacks'
+					Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+				end
+			end,
+		})
+	end,
+	-- keys = {
+	-- 	{ "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+	-- },
 }
