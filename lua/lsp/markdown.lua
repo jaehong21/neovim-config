11941302184d4c209f6b470f48b77b2330ad1a75
jaehong21 <dev@jaehong21.com>
2025-11-27 18:49:19 +0900

    chore: disable

--- lua/lsp/markdown.lua
+++ lua/lsp/markdown.lua
@@ -45,6 +45,7 @@ return {
 
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
+		enabled = false,
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons", -- if you prefer nvim-web-devicons
